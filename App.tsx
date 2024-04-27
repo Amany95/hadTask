75124591c4e8972adfafc7da92664c22b688fd6d
amany adel <36702658+Amany95@users.noreply.github.com>
2024-04-27 16:09:21 +0300

    add bottom navigator

--- App.tsx
+++ App.tsx
@@ -9,8 +9,21 @@ import React from 'react';
 
 import {Home} from './src/containers/home/screens/home/Home';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 function App(): React.JSX.Element {
-  return <Home />;
+  return (
+    <NavigationContainer>
+          <Tab.Navigator>
+ 
+ <Tab.Screen name="Home" component={Home} />
+
+
+</Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
