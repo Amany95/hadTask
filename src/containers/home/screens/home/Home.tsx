20c274abcfe91c0a14da8bbcb82cf59388b88015
amany adel <36702658+Amany95@users.noreply.github.com>
2024-04-27 15:35:02 +0300

    add code structure

---
+++ src/containers/home/screens/home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function Home(): JSX.Element {
+  return <Text>Home screen</Text>;
+}
+
+export {Home};
---
+++ src/containers/movies/screens/seeAll/SeeAll.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function SeeAll(): JSX.Element {
+  return <Text>See All screen</Text>;
+}
+
+export {SeeAll};
